e8b0e4b84ce8f21a249032d85956684d2a115c9f
ronak <ronak9896@gmail.com>
2019-10-26 04:15:33 -0400

    Adding recycler view to the data

--- app/src/main/java/com/example/leetcoderecommendation/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/leetcoderecommendation/ui/dashboard/DashboardFragment.java
@@ -11,25 +11,23 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.leetcoderecommendation.R;
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+    private RecyclerView recyclerView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        recyclerView = root.findViewById(R.id.leaderboard_recycler_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter();
+        recyclerView.setAdapter(adapter);
         return root;
     }
 }
